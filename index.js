6303ddeba156b2e705d9b7426a92aa71e1911343
Elsa Magalhaes <elsa.magalhaes@gmail.com>
2021-09-22 11:32:56 +0200

    mongoose

---
+++ index.js
@@ -0,0 +1,24 @@
+// import the NPM dependancy package
+const express     = require("express");
+const bodyParser  = require("body-parser");
+const mongoose    = require("mongoose");
+
+// initialise express() inside of your app variable
+const app = express();
+
+// parse body of incoming json requests
+app.use(bodyParser.json());
+
+// import route- and model modules and pass your app
+require("./models/Pet")(app);
+require("./routes/petRoutes")(app);
+
+mongoose.connect("mongodb+srv://Admin:<password>@cluster0.tcbrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+// choose what port on which to run the server
+const PORT = 5000;
+
+// use the app variable and listen on the port
+app.listen(PORT, () => {
+  console.log(`Server running`);
+});
